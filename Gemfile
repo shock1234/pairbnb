a80b705e09935f691d320dfad0fc226497369463
shock1234 <perfect.1988@hotmail.com>
2017-03-23 10:29:13 +0800

    done fb sign in and clearance

--- Gemfile
+++ Gemfile
@@ -5,6 +5,16 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+# User authentication
+gem 'clearance'
+
+# Privacy Protection
+gem 'figaro'
+
+# Facebook Log in
+gem 'omniauth'
+
+gem 'omniauth-facebook'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,44 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  root 'home#index'
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+
+  resources :users, only: [:show, :edit, :update, :delete]
+
+
+# /Clearance Document
+  # constraints Clearance::Constraints::SignedIn.new { |user| user.admin? } do
+  #   root to: "admin/dashboards#show", as: :admin_root
+  # end
+
+  # constraints Clearance::Constraints::SignedIn.new do
+  #   root to: "dashboards#show", as: :signed_in_root
+  # end
+
+  # constraints Clearance::Constraints::SignedOut.new do
+  #   root to: "marketing#index"
+  # end
+# /end
+
+
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  # resources generate id, resource generate no id
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+
+
+
+  #set up a route in our app that matches to the callback URL, i.e. a URL that redirects the user from Facebook to your app
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 end
